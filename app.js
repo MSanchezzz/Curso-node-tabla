24215a02f609a7ffb41d967152ad7b1bcb1fbb2d
Matias Sanchez <sanchez.matias153@gmail.com>
2023-05-16 19:24:50 -0400

    Primer commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
+const colors = require('colors');
+
+
+
+console.clear();
+
+//const [ , , arg3 = 'base=5'  ] = process.argv;
+//const [ , base = 5] = arg3.split('=');
+//const base = 5;
+
+
+crearArchivo ( argv.b, argv.l, argv.h )
+   .then( nombreArchivo => console.log(nombreArchivo,'creado'.green))
+   .catch(err => console.log(err));
